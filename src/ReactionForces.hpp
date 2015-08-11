60bcee368a3a1a9473f569f81163041180551f7c
Javier Hidalgo <javier.hidalgo_carrio@dfki.de>
2015-08-11 12:17:44 +0200

    src: added the torque computation for exoter  (experimental) and asguard to compare. test: simple unit test

--- src/ReactionForces.hpp
+++ src/ReactionForces.hpp
@@ -4,7 +4,10 @@
 #include <vector> /** std vector **/
 #include <Eigen/Core>/** Eigen core library **/
 #include <Eigen/StdVector> /** For STL container with Eigen types **/
-#include <base/eigen.h>
+
+/** Base types **/
+#include <base/Eigen.hpp>
+#include <base/Pose.hpp>
 
 #include <iostream>
 
@@ -15,10 +18,10 @@ namespace dynamics
 
     enum wheelIdx
     {
-	FRONT_LEFT   = 0,
-	FRONT_RIGHT  = 1,
-	MIDDLE_LEFT = 2,
-	MIDDLE_RIGHT  = 3,
+        FRONT_LEFT   = 0,
+        FRONT_RIGHT  = 1,
+        MIDDLE_LEFT = 2,
+        MIDDLE_RIGHT  = 3,
         REAR_LEFT = 4,
         REAR_RIGHT = 5
     };
@@ -32,23 +35,29 @@ namespace dynamics
         static void forceAnalysis (const base::Vector3d &centerOfMass,
                 const std::vector< Eigen::Matrix<double, 3, 1>, Eigen::aligned_allocator < Eigen::Matrix<double, 3, 1> > > &footPosition,
                 const Eigen::Quaterniond &orient,
-                const double gravity, Eigen::Matrix<double, NUMBER_OF_WHEELS, 1> &forces)
+                const double gravity,
+                Eigen::Matrix<double, NUMBER_OF_WHEELS, 1> &forces)
         {
-            register int k = 0;
             std::vector< Eigen::Matrix<double, 3, 1> , Eigen::aligned_allocator < Eigen::Matrix<double, 3, 1> > > foot = footPosition; //! foot position w.r.t the Center of Gravity
             Eigen::Matrix<double, NUMBER_OF_WHEELS, NUMBER_OF_WHEELS> A; //! Matrix A of system of equations Ax=b
             Eigen::Matrix<double, NUMBER_OF_WHEELS, 1> b; //! Vector b in Ax=b
             Eigen::Matrix<double, 3, 1> euler;
             Eigen::Quaterniond attitude;
 
+            /** Eliminate heading we are only interested on Pitch and Roll **/
             euler[2] = 0.00;
-            euler[1] = orient.toRotationMatrix().eulerAngles(2,1,0)[1];//Pitch
-            euler[0] = orient.toRotationMatrix().eulerAngles(2,1,0)[2];//Roll
+            euler[1] = base::getEuler(orient)[1];//Pitch
+            euler[0] = base::getEuler(orient)[2];//Roll
 
             /** Attitude **/
             attitude = Eigen::Quaterniond(Eigen::AngleAxisd(euler[2], Eigen::Vector3d::UnitZ())*
                 Eigen::AngleAxisd(euler[1], Eigen::Vector3d::UnitY()) *
                 Eigen::AngleAxisd(euler[0], Eigen::Vector3d::UnitX()));
+
+            /** Attitude in rotation matrix form **/
+            std::cout<<"R:\n"<<attitude.toRotationMatrix()<<"\n";
+            Eigen::Vector3d r = attitude.toRotationMatrix().col(2);
+            std::cout<<"r vector:\n"<<r<<"\n";
 
             /** First row is all ones: Constraint 1 [1, 1, 1, 1, 1, 1] **/
             A.row(0).setOnes();
@@ -56,25 +65,23 @@ namespace dynamics
             /** b vector **/
             b << gravity, 0.00, 0.00, 0.00, 0.00, 0.00;
 
+            register int k = 0;
             for (std::vector< Eigen::Matrix<double, 3, 1> , Eigen::aligned_allocator < Eigen::Matrix<double, 3, 1> > > ::iterator it = foot.begin(); it != foot.end(); ++it)
             {
-                /** The foot position is w.r.t. the body center. Make it to the Center of Gravity **/
+                /** The foot position is w.r.t. the geometrical body center (B_CoM). Make it to the Center of Gravity in case it is different **/
                 *it -= centerOfMass;
 
-                /** Rotate to the world fixed frame (inertial frame) **/
-                *it = attitude * (*it);
+                std::cout<<"*it:\n"<<*it<<"\n";
 
-                Eigen::Matrix3d cross2product; /** Crossproduct  matrix */
-                cross2product << 0, -(*it)(2), (*it)(1),
-                                 (*it)(2), 0, -(*it)(0),
-                                 -(*it)(1), (*it)(0), 0;
+                /** Cross product to compute the torque **/
+                Eigen::Vector3d torque = (*it).cross(r);
 
                 /** Take the z elements **/
-                A(1,k) = cross2product(0,2);// Constraint 1: Torque along X in wheels [0..3] is zero [-r0x, -r1x, -r2x, -r3x, -r4x, -r5x]
-                A(2,k) = cross2product(1,2);// Constraint 2: Torque along Y in wheels [0,2] is zero
-                A(3,k) = cross2product(1,2);// Constraint 3: Torque along Y in wheels [1,3] is zero
-                A(4,k) = cross2product(0,2);// Constraint 4: Torque along X in wheels [4,5] is zero
-                //A(5,k) = cross2product(0,2);// Constraint 5: Torque along X is zero for the second body (rear part of Asguard)
+                A(1,k) = torque[0];// Constraint 1: Torque along X in wheels [0..3] is zero [-r0x, -r1x, -r2x, -r3x, -r4x, -r5x] (we will change the two last element to zero afterwards)
+                A(2,k) = torque[1];// Constraint 2: Torque along Y in wheels [0,2] is zero
+                A(3,k) = torque[1];// Constraint 3: Torque along Y in wheels [1,3] is zero
+                A(4,k) = torque[0];// Constraint 4: Torque along X in wheels [4,5] is zero
+                A(5,k) = torque[1];// Constraint 5: Torque along Y for all wheels is zero
 
                 k++;
             }
@@ -85,12 +92,79 @@ namespace dynamics
             A(4,0) = 0.00; A(4,1) = 0.00; A(4,2) = 0.00; A(4,3) = 0.00;// Constraint 4:
 
             std::cout<<"A is \n"<<A<<"\n";
+            std::cout<<"b is \n"<<b<<"\n";
 
             /** Solve the system by LU decomposition **/
             forces = A.fullPivLu().solve(b);
 
             return;
         };
+
+
+        static void forceAnalysis (const base::Vector3d &centerOfMass,
+                const std::vector< Eigen::Matrix<double, 3, 1>, Eigen::aligned_allocator < Eigen::Matrix<double, 3, 1> > > &footPosition,
+                const Eigen::Quaterniond &orient,
+                const double gravity,
+                Eigen::Matrix<double, 4, 1> &forces)
+        {
+            std::vector< Eigen::Matrix<double, 3, 1> , Eigen::aligned_allocator < Eigen::Matrix<double, 3, 1> > > foot = footPosition; //! foot position w.r.t the Center of Gravity
+            Eigen::Matrix<double, 4, 4> A; //! Matrix A of system of equations Ax=b
+            Eigen::Matrix<double, 4, 1> b; //! Vector b in Ax=b
+            Eigen::Matrix<double, 3, 1> euler;
+            Eigen::Quaterniond attitude;
+
+            /** Eliminate heading we are only interested on Pitch and Roll **/
+            euler[2] = 0.00;
+            euler[1] = base::getEuler(orient)[1];//Pitch
+            euler[0] = base::getEuler(orient)[2];//Roll
+
+            /** Attitude **/
+            attitude = Eigen::Quaterniond(Eigen::AngleAxisd(euler[2], Eigen::Vector3d::UnitZ())*
+                Eigen::AngleAxisd(euler[1], Eigen::Vector3d::UnitY()) *
+                Eigen::AngleAxisd(euler[0], Eigen::Vector3d::UnitX()));
+
+            /** Attitude in rotation matrix form **/
+            std::cout<<"R:\n"<<attitude.toRotationMatrix()<<"\n";
+            Eigen::Vector3d r = attitude.toRotationMatrix().col(2);
+            std::cout<<"r vector:\n"<<r<<"\n";
+
+            /** First row is all ones: Constraint 1 [1, 1, 1, 1, 1, 1] **/
+            A.row(0).setOnes();
+
+            /** b vector **/
+            b << gravity, 0.00, 0.00, 0.00;
+
+            register int k = 0;
+            for (std::vector< Eigen::Matrix<double, 3, 1> , Eigen::aligned_allocator < Eigen::Matrix<double, 3, 1> > > ::iterator it = foot.begin(); it != foot.end(); ++it)
+            {
+                /** The foot position is w.r.t. the geometrical body center (B_CoM). Make it to the Center of Gravity in case it is different **/
+                *it -= centerOfMass;
+
+                std::cout<<"*it:\n"<<*it<<"\n";
+
+                /** Cross product to compute the torque **/
+                Eigen::Vector3d torque = (*it).cross(r);
+
+                /** Take the z elements **/
+                A(1,k) = torque[1];// Constraint 2: Torque along Y is zero
+                A(2,k) = torque[0];// Constraint 3: Torque along X in wheels [0,1] is zero
+                A(3,k) = torque[0];// Constraint 4: Torque along X in wheels [2,3] is zero
+
+                k++;
+            }
+            /** Set to zero the not requires elements **/
+            A(2,2) = 0.00; A(2,3) = 0.00;// Constraint 3:
+            A(3,0) = 0.00; A(3,1) = 0.00; // Constraint 4:
+
+            std::cout<<"A is \n"<<A<<"\n";
+            std::cout<<"b is \n"<<b<<"\n";
+
+            /** Solve the system by LU decomposition **/
+            forces = A.fullPivLu().solve(b);
+
+            return;
+        };
+
     };
 }
 
--- test/test_ReactionForces.cpp
+++ test/test_ReactionForces.cpp
@@ -3,15 +3,72 @@
 
 using namespace dynamics;
 
+static const double D2R = M_PI/180.00; /** Convert degree to radian **/
+static const double R2D = 180.00/M_PI; /** Convert radian to degree **/
+
 BOOST_AUTO_TEST_CASE(test_for_reaction_forces)
 {
     std::vector< Eigen::Matrix<double, 3, 1> , Eigen::aligned_allocator < Eigen::Matrix<double, 3, 1> > > chainPosition; /** Chain position of contact points **/
     Eigen::Matrix<double, NUMBER_OF_WHEELS, 1> forces; /** forces to calculate */
-    base::Vector3d centerOfMass;
+    base::Vector3d centerOfMass, euler;
     Eigen::Quaterniond orientation;
+
+    centerOfMass.setZero();
+    euler.setZero();
+    euler[0] = -0.00 * D2R;
+
+    orientation = Eigen::Quaterniond(Eigen::AngleAxisd(euler[2], Eigen::Vector3d::UnitZ())*
+        Eigen::AngleAxisd(euler[1], Eigen::Vector3d::UnitY()) *
+        Eigen::AngleAxisd(euler[0], Eigen::Vector3d::UnitX()));
+
+    std::cout<<"ORIENTATION ROLL: "<< euler[0] <<" PITCH: "<< euler[1] <<" YAW: "<< euler[2] <<"\n";
 
     chainPosition.resize(NUMBER_OF_WHEELS);
 
+    /** Set the position in a square [-1, 1] **/
+    chainPosition[0] << 1.0, 1.0, -1.0;
+    chainPosition[1] << 1.0, -1.0, -1.0;
+    chainPosition[2] << 0.0, 1.0, -1.0;
+    chainPosition[3] << 0.0, -1.0, -1.0;
+    chainPosition[4] << -1.0, 1.0, -1.0;
+    chainPosition[5] << -1.0, -1.0, -1.0;
+
+    /** Compute normal forces **/
     ::dynamics::ReactionForces::forceAnalysis(centerOfMass, chainPosition, orientation, 9.81, forces);
+    std::cout<<"FORCES\n"<<forces<<"\n";
+
+}
+
+
+BOOST_AUTO_TEST_CASE(test_for_reaction_forces_asguard)
+{
+    std::vector< Eigen::Matrix<double, 3, 1> , Eigen::aligned_allocator < Eigen::Matrix<double, 3, 1> > > chainPosition; /** Chain position of contact points **/
+    Eigen::Matrix<double, 4, 1> forces; /** forces to calculate */
+    base::Vector3d centerOfMass, euler;
+    Eigen::Quaterniond orientation;
+
+    std::cout<<"** ASGUARD **\n";
+
+    centerOfMass.setZero();
+    euler.setZero();
+    euler[0] = 0.00 * D2R;
+
+    orientation = Eigen::Quaterniond(Eigen::AngleAxisd(euler[2], Eigen::Vector3d::UnitZ())*
+        Eigen::AngleAxisd(euler[1], Eigen::Vector3d::UnitY()) *
+        Eigen::AngleAxisd(euler[0], Eigen::Vector3d::UnitX()));
+
+    std::cout<<"ORIENTATION ROLL: "<< euler[0] <<" PITCH: "<< euler[1] <<" YAW: "<< euler[2] <<"\n";
+
+    chainPosition.resize(4);
+
+    /** Set the position in a square [-1, 1] **/
+    chainPosition[0] << 1.0, 1.0, -1.0;
+    chainPosition[1] << 1.0, -1.0, -1.0;
+    chainPosition[2] << -1.0, 1.0, -1.0;
+    chainPosition[3] << -1.0, -1.0, -1.0;
+
+    /** Compute normal forces **/
+    ::dynamics::ReactionForces::forceAnalysis(centerOfMass, chainPosition, orientation, 9.81, forces);
+    std::cout<<"FORCES\n"<<forces<<"\n";
 
 }
